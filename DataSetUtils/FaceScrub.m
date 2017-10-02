c67caf51e9a7d6283ff8191a19370e9f7660800a
jfrancis71 <julian.w.francis@gmail.com>
2017-10-02 16:46:28 +0100

    initial

---
+++ DataSetUtils/FaceScrub.m
@@ -0,0 +1,40 @@
+(* ::Package:: *)
+
+(* Suggested use:
+files=FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\ActorImages1\\*.jpg"];Length[files]
+p1 = Partition[files,UpTo[100]] ;
+images = Flatten[CZGetImagesPartition[ p1 ]];
+faceScrub = CZImportFaceScrub[ files, images, CZReadFaceScrubDatabase[ "C:\\Users\\julian\\ImageDataSets\\FaceScrub\\facescrub_actors.txt"] ];
+*)
+
+
+CZReadFaceScrubDatabase[srcFile_] := ReadList[StringToStream[Import[srcFile]],String,RecordSeparators->"\n"];
+
+
+CZGetURL[recordNo_Integer,database_] := (
+   fileName=ReadList[StringToStream[database[[recordNo]]],Record,RecordSeparators->"\t"][[4]];
+   Import[fileName]
+)
+
+
+CZGetName[recordNo_Integer,database_] := ReadList[StringToStream[database[[recordNo]]],Record,RecordSeparators->"\t"][[1]]
+
+
+CZBoundingBox[recordNo_Integer,database_,image_] := ( 
+   c=ReadList[StringToStream[database[[recordNo]]],Record,RecordSeparators->"\t"][[5]];
+   p=Map[FromDigits,ReadList[StringToStream[c],Record,RecordSeparators->","]];
+   {{p[[1]],ImageDimensions[image][[2]]-p[[2]]},{p[[3]],ImageDimensions[image][[2]]-p[[4]]}})
+
+
+(* Implemented as Table as opposed to Map. Easier to track progress *)
+CZImportFaceScrub[files_,images_,database_] := (
+   Assert[ Length[files] == Length[images] ];
+   Table[{images[[f]],CZGetName[FromDigits[FileBaseName[files[[f]]]],database],CZBoundingBox[FromDigits[FileBaseName[files[[f]]]],database,images[[f]]]},{f,1,Length[files]}]
+)
+
+
+(* Internal function to deal with speed issues on loading many images *)
+CZGetImagesPartition[setOfFiles_] := Table[Map[Import,setOfFiles[[f]]],{f,1,Length[setOfFiles]}];
+
+
+CZGetImages[files_] := CZGetImagesPartition[ Partition[files,UpTo[100]] ]
