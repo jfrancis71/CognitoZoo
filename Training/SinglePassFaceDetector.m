5684d5e02267a8126a3fa10687fbe501863eea25
jfrancis71 <julian.w.francis@gmail.com>
2018-05-14 08:35:19 +0100

    doing full out of core training.

--- Training/SinglePassFaceDetector.m
+++ Training/SinglePassFaceDetector.m
@@ -1,5 +1,24 @@
 (* ::Package:: *)
 
+(*
+   Training file for a single pass face detector.
+   Input image should be of size VGA.
+   
+   Please note, have had some problems regarding training bug, please ensure that no other kernel
+   instances are running, and may wish to try CPU. Not sure if it is a GPU/CPU issue, or problems
+   with decoding as we are doing out of core training.
+*)
+
+
+(*
+   H.-W. Ng, S. Winkler.
+   A data-driven approach to cleaning large face datasets.
+   Proc. IEEE International Conference on Image Processing (ICIP), Paris, France, Oct. 27-30, 2014.
+
+   http://vintage.winklerbros.net/facescrub.html
+*)
+
+
 sz1[face_]:=If[size[face]>190,2,1];
 
 
@@ -15,6 +34,42 @@ Ceiling[(#[[1,1]]+#[[2,1]])/(2*32)]
 
 files=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\ActorImages1\\*.jpg"]];
 faces=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\DLibFaces1.mx"];
+
+
+mfiles1=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\ActorImages1\\*.jpg"]];
+mfaces1=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\DLibFaces1.mx"];
+mfiles2=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\ActorImages2\\*.jpg"]];
+mfaces2=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\DLibFaces2.mx"];
+mfiles3=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\ActorImages3\\*.jpg"]];
+mfaces3=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\DLibFaces3.mx"];
+mfiles4=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\ActorImages4\\*.jpg"]];
+mfaces4=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\DLibFaces4.mx"];
+mfiles5=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\ActorImages5\\*.jpg"]];
+mfaces5=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\DLibFaces5.mx"];
+mfiles6=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\ActorImages6\\*.jpg"]];
+mfaces6=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActorImages\\VGA\\DLibFaces6.mx"];
+mfiles=Join[mfiles1,mfiles2,mfiles3,mfiles4,mfiles5,mfiles6];
+mfaces=Join[mfaces1,mfaces2,mfaces3,mfaces4,mfaces5,mfaces6];
+
+
+ffiles1=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\ActressImages1\\*.jpg"]];
+ffaces1=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\DLibFaces1.mx"];
+ffiles2=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\ActressImages2\\*.jpg"]];
+ffaces2=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\DLibFaces2.mx"];
+ffiles3=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\ActressImages3\\*.jpg"]];
+ffaces3=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\DLibFaces3.mx"];
+ffiles4=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\ActressImages4\\*.jpg"]];
+ffaces4=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\DLibFaces4.mx"];
+ffiles5=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\ActressImages5\\*.jpg"]];
+ffaces5=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\DLibFaces5.mx"];
+ffiles6=Map[File,FileNames["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\ActressImages6\\*.jpg"]];
+ffaces6=Import["C:\\Users\\julian\\ImageDataSets\\FaceScrub\\ActressImages\\VGA\\DLibFaces6.mx"];
+ffiles=Join[ffiles1,ffiles2,ffiles3,ffiles4,ffiles5,ffiles6];
+ffaces=Join[ffaces1,ffaces2,ffaces3,ffaces4,ffaces5,ffaces6];
+
+
+files=Join[mfiles1,mfiles2,mfiles3,mfiles4];
+faces=Join[mfaces1,mfaces2,mfaces3,mfaces4];
 
 
 dataset=RandomSample[Table[files[[f]]->faces[[f]],{f,1,Length[faces]}]];
@@ -39,16 +94,13 @@ net = NetGraph[
 ];   
 
 
-trained=NetTrain[net,ndataset[[1;;7000]],ValidationSet->ndataset[[7001;;-1]],TargetDevice->"GPU"];
+trained=NetTrain[net,ndataset[[1;;80000]],All,ValidationSet->ndataset[[80001;;-1]],TargetDevice->"GPU",TrainingProgressCheckpointing->{"Directory","c:\\users\\julian\\checkpoints"}];
 
 
-(* 7,000
-   Significant learning within 1 batch. Achieved loss .00488, error 0.136%
-   Stopped after 3 training rounds (still learning)
-*)
+(* Achieves around 0.1% error rate. Should learn quite quickly within first round, five rounds is optimal. In practice works quite well *)
 
 
-Export["c:\\Users\\julian\\Google Drive\\Personal\\Computer Science\\CZModels\\SinglePassTmp.wlnet",trained];
+Export["c:\\Users\\julian\\Google Drive\\Personal\\Computer Science\\CZModels\\SinglePassTmp.mx",trained];
 
 
 sz[1]:=57/(32*2.)
