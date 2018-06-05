f17ab72b6d526953b8baa0889ffd53be10af6a0b
Julian Francis <julian.w.francis@gmail.com>
2018-06-05 13:30:42 +0100

    renaming encoders/decoders reverse adjective/noun
    Better English plus plays better with tooltips

--- CZSSD.m
+++ CZSSD.m
@@ -61,7 +61,7 @@ Options[ CZDetectObjects ] = {
 TargetDevice->"CPU"
 };
 CZDetectObjects[ image_, opts:OptionsPattern[] ] :=
-   CZNonMaxSuppressionPerClass@CZDeconformObjects[ CZOutputDecode@SSDNet[ CZInputEncode@CZConformImage[image,{300,300},"Stretch"], opts ], image, {300, 300}, "Stretch"  ]
+   CZNonMaxSuppressionPerClass@CZDeconformObjects[ CZDecodeOutput@SSDNet[ CZEncodeInput@CZConformImage[image,{300,300},"Stretch"], opts ], image, {300, 300}, "Stretch"  ]
 
 
 Options[ CZHighlightObjects ] = Options[ CZDetectObjects ];
@@ -116,11 +116,11 @@ anchorsh6 = {0.870,0.955,0.615,1.230};
 SSDNet=Import["CZModels/SSDVGG300.wlnet"];
 
 
-CZInputEncode[ image_ ] :=
+CZEncodeInput[ image_ ] :=
    (ImageData[image,Interleaving->False]*256)-{123,117,104};
 
 
-CZOutputDecode[locs_, probs_,{anchorsx_,anchorsy_,anchorsw_,anchorsh_}]:=( 
+CZDecodeOutput[locs_, probs_,{anchorsx_,anchorsy_,anchorsw_,anchorsh_}]:=( 
 (* slocs format A{xywh}HW *)
    slocs=Partition[locs,4];
    rec=Position[probs,x_/;x>.5];
@@ -140,12 +140,12 @@ CZOutputDecode[locs_, probs_,{anchorsx_,anchorsy_,anchorsw_,anchorsh_}]:=(
 )
 
 
-CZOutputDecode[ netOutput_ ] :=
+CZDecodeOutput[ netOutput_ ] :=
    Join[
-      CZOutputDecode[netOutput["Locs1"],netOutput["ObjMap1"][[All,All,All,2;;21]],{anchorsx1,anchorsy1,anchorsw1,anchorsh1}],
-      CZOutputDecode[netOutput["Locs2"],netOutput["ObjMap2"][[All,All,All,2;;21]],{anchorsx2,anchorsy2,anchorsw2,anchorsh2}],
-      CZOutputDecode[netOutput["Locs3"],netOutput["ObjMap3"][[All,All,All,2;;21]],{anchorsx3,anchorsy3,anchorsw3,anchorsh3}],
-      CZOutputDecode[netOutput["Locs4"],netOutput["ObjMap4"][[All,All,All,2;;21]],{anchorsx4,anchorsy4,anchorsw4,anchorsh4}],
-      CZOutputDecode[netOutput["Locs5"],netOutput["ObjMap5"][[All,All,All,2;;21]],{anchorsx5,anchorsy5,anchorsw5,anchorsh5}],
-      CZOutputDecode[netOutput["Locs6"],netOutput["ObjMap6"][[All,All,All,2;;21]],{anchorsx6,anchorsy6,anchorsw6,anchorsh6}]
+      CZDecodeOutput[netOutput["Locs1"],netOutput["ObjMap1"][[All,All,All,2;;21]],{anchorsx1,anchorsy1,anchorsw1,anchorsh1}],
+      CZDecodeOutput[netOutput["Locs2"],netOutput["ObjMap2"][[All,All,All,2;;21]],{anchorsx2,anchorsy2,anchorsw2,anchorsh2}],
+      CZDecodeOutput[netOutput["Locs3"],netOutput["ObjMap3"][[All,All,All,2;;21]],{anchorsx3,anchorsy3,anchorsw3,anchorsh3}],
+      CZDecodeOutput[netOutput["Locs4"],netOutput["ObjMap4"][[All,All,All,2;;21]],{anchorsx4,anchorsy4,anchorsw4,anchorsh4}],
+      CZDecodeOutput[netOutput["Locs5"],netOutput["ObjMap5"][[All,All,All,2;;21]],{anchorsx5,anchorsy5,anchorsw5,anchorsh5}],
+      CZDecodeOutput[netOutput["Locs6"],netOutput["ObjMap6"][[All,All,All,2;;21]],{anchorsx6,anchorsy6,anchorsw6,anchorsh6}]
 ];
--- CZVisiNet.m
+++ CZVisiNet.m
@@ -1,27 +1,6 @@
 (* ::Package:: *)
 
-<<CZUtils.m
-
-
-trained = Import["CZModels/VisiNetv1.wlnet"];
-
-
-CZOutputDecode[ assoc_, threshold_ ] := Join[
-   Map[{Extract[assoc["FaceArray1"],#],Rectangle[{32*(#[[2]]-.5),480-32*(#[[1]]-.5)}-{37,37},{32*(#[[2]]-.5),480-32*(#[[1]]-.5)}+{37,37}]}&,Position[assoc["FaceArray1"],x_/;x>threshold]],
-   Map[{Extract[assoc["FaceArray2"],#],Rectangle[{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}-{65,65},{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}+{65,65}]}&,Position[assoc["FaceArray2"],x_/;x>threshold]],
-   Map[{Extract[assoc["FaceArray3"],#],Rectangle[{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}-{100,100},{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}+{100,100}]}&,Position[assoc["FaceArray3"],x_/;x>threshold]],
-   Map[{Extract[assoc["FaceArray1Offset"],#],Rectangle[{32*(#[[2]]),480-32*(#[[1]]-1)}-{37,37},{32*(#[[2]]),480-32*(#[[1]]-1)}+{37,37}]}&,Position[assoc["FaceArray1Offset"],x_/;x>threshold]],
-   Map[{Extract[assoc["FaceArray2Offset"],#],Rectangle[{64*(#[[2]]),480-64*(#[[1]]-1)}-{65,65},{64*(#[[2]]),480-64*(#[[1]]-1)}+{65,65}]}&,Position[assoc["FaceArray2Offset"],x_/;x>threshold]],
-   Map[{Extract[assoc["FaceArray3Offset"],#],Rectangle[{64*(#[[2]]),480-64*(#[[1]]-1)}-{100,100},{64*(#[[2]]),480-64*(#[[1]]-1)}+{100,100}]}&,Position[assoc["FaceArray3Offset"],x_/;x>threshold]]
-]
-
-
-Options[ CZDetectFaces ] = {
-   Threshold->0.5,
-   TargetDevice->"CPU"
-};
-CZDetectFaces[ img_Image, opts:OptionsPattern[] ] := 
-   CZNonMaxSuppression@CZDeconformObjects[ CZOutputDecode[ trained[ CZConformImage[img,{640,480},"Fit"], TargetDevice->OptionValue["TargetDevice"] ], OptionValue["Threshold"] ], img, {640,480}, "Fit" ];
+(* Public Interface *)
 
 
 Options[ CZHighlightFaces ] = {
@@ -29,3 +8,30 @@ Options[ CZHighlightFaces ] = {
    Threshold->0.5
 };
 CZHighlightFaces[ img_Image, opts:OptionsPattern[] ] := HighlightImage[ img, CZDetectFaces[ img, opts ] ]
+
+
+Options[ CZDetectFaces ] = {
+   Threshold->0.5,
+   TargetDevice->"CPU"
+};
+CZDetectFaces[ img_Image, opts:OptionsPattern[] ] := 
+   CZNonMaxSuppression@CZDeconformObjects[ CZDecodeOutput[ trained[ CZConformImage[img,{640,480},"Fit"], TargetDevice->OptionValue["TargetDevice"] ], OptionValue["Threshold"] ], img, {640,480}, "Fit" ];
+
+
+(* Private Code *)
+
+
+<<CZUtils.m
+
+
+trained = Import["CZModels/VisiNetv1.wlnet"];
+
+
+CZDecodeOutput[ assoc_, threshold_ ] := Join[
+   Map[{Extract[assoc["FaceArray1"],#],Rectangle[{32*(#[[2]]-.5),480-32*(#[[1]]-.5)}-{37,37},{32*(#[[2]]-.5),480-32*(#[[1]]-.5)}+{37,37}]}&,Position[assoc["FaceArray1"],x_/;x>threshold]],
+   Map[{Extract[assoc["FaceArray2"],#],Rectangle[{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}-{65,65},{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}+{65,65}]}&,Position[assoc["FaceArray2"],x_/;x>threshold]],
+   Map[{Extract[assoc["FaceArray3"],#],Rectangle[{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}-{100,100},{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}+{100,100}]}&,Position[assoc["FaceArray3"],x_/;x>threshold]],
+   Map[{Extract[assoc["FaceArray1Offset"],#],Rectangle[{32*(#[[2]]),480-32*(#[[1]]-1)}-{37,37},{32*(#[[2]]),480-32*(#[[1]]-1)}+{37,37}]}&,Position[assoc["FaceArray1Offset"],x_/;x>threshold]],
+   Map[{Extract[assoc["FaceArray2Offset"],#],Rectangle[{64*(#[[2]]),480-64*(#[[1]]-1)}-{65,65},{64*(#[[2]]),480-64*(#[[1]]-1)}+{65,65}]}&,Position[assoc["FaceArray2Offset"],x_/;x>threshold]],
+   Map[{Extract[assoc["FaceArray3Offset"],#],Rectangle[{64*(#[[2]]),480-64*(#[[1]]-1)}-{100,100},{64*(#[[2]]),480-64*(#[[1]]-1)}+{100,100}]}&,Position[assoc["FaceArray3Offset"],x_/;x>threshold]]
+]
