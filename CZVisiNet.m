393d1f83c665efc37074182df38637c8ffdebde2
jfrancis71 <julian.w.francis@gmail.com>
2018-07-04 11:41:46 +0100

    Adding XGA capability
    Adding option to analyse image at XGA resolution; should enable
    detections of faces around half the size relative to the VGA version

--- CZVisiNet.m
+++ CZVisiNet.m
@@ -6,16 +6,20 @@
 <<CZUtils.m
 
 
+SyntaxInformation[ Detail ]= {"ArgumentsPattern"->{_}};
 Options[ CZDetectFaces ] = Join[{
    TargetDevice->"CPU",
-   Threshold->.5
+   Threshold->.5,
+   Detail->"XGA"
 }, Options[ CZNonMaxSuppression ] ];
 
 
 CZDetectFaces[ image_Image, opts:OptionsPattern[] ] :=
    CZNonMaxSuppression[FilterRules[ {opts}, Options[ CZNonMaxSuppression ] ] ]@
-      CZObjectsDeconformer[ image, {640, 480}, "Fit" ]@CZOutputDecoder[ OptionValue[ Threshold ] ]@
-            (trained[ #, TargetDevice->OptionValue[ TargetDevice ] ]&)@CZImageConformer[{640,480},"Fit"]@image;
+   If[OptionValue[Detail]=="VGA",
+      CZObjectsDeconformer[ image, {640, 480}, "Fit" ]@(CZVGADetectFaces[#,FilterRules[{opts},Options[ CZVGADetectFaces ] ] ]&)@CZImageConformer[{640,480},"Fit"]@image,
+      CZObjectsDeconformer[ image, {1280, 960}, "Fit" ]@(CZXGADetectFaces[#,FilterRules[{opts},Options[ CZXGADetectFaces ] ] ]&)@CZImageConformer[{1280,960},"Fit"]@image
+   ]   
 
 
 Options[ CZHighlightFaces ] = Options[ CZDetectFaces ];
@@ -23,6 +27,40 @@ CZHighlightFaces[ img_Image, opts:OptionsPattern[] ] := HighlightImage[ img, CZD
 
 
 (* Private Code *)
+
+
+Options[ CZVGADetectFaces ] = {
+   TargetDevice->"CPU",
+   Threshold->.5
+};
+CZVGADetectFaces[ image_Image, opts:OptionsPattern[] ] :=
+   CZOutputDecoder[ OptionValue[ Threshold ] ]@(trained[ #, TargetDevice->OptionValue[ TargetDevice ] ]&)@image;
+
+
+CZMapAt[_,{},_]:={};
+CZMapAt[f_,list_,spec_]:=MapAt[f,list,spec]
+
+
+Options[ CZXGADetectFaces ] = {
+   TargetDevice->"CPU",
+   Threshold->.5
+};
+CZXGADetectFaces[ image_Image, opts:OptionsPattern[] ] := Join[
+   CZObjectsDeconformer[ image, {640, 480}, "Fit" ]@CZVGADetectFaces[ CZImageConformer[{640,480},"Fit"]@image, opts ],
+   Flatten[MapThread[
+      Function[{objects,offset},CZMapAt[(#+offset)&,objects,{All,2,All}]],{
+      Map[CZVGADetectFaces,ImageTrim[image,{
+      {{1,1},{640,480}},(*bottom left*)
+      {{1,481},{641,960}},(*top left*)
+      {{641,1},{1280,480}},(*bottom right*)
+     {{641,481},{1280,960}},(*top right*)
+     {{320,240},{960,720}},(*centre*)
+     {{1,240},{640,720}},(*centre left*)
+     {{641,240},{1280,720}}(*centre right*)
+   }]],
+   {{0,0},{0,480},{640,0},{640,480},{320,240},{0,240},{640,240}}
+   }],1]
+];
 
 
 trained = Import[LocalCache@CloudObject["https://www.wolframcloud.com/objects/julian.w.francis/VisiNetv1"],"WLNet"];
