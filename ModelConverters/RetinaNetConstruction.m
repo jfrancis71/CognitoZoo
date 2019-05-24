0bc6b10ab6bb159d10e1b20ccdbe82e314ad1d5e
Julian Francis <julian.w.francis@gmail.com>
2019-05-24 15:22:25 +0100

    tidying up names

--- ModelConverters/RetinaNetConstruction.m
+++ ModelConverters/RetinaNetConstruction.m
@@ -1,39 +1,39 @@
 (* ::Package:: *)
 
-BNConvolutionLayer[ outputChannels_Integer, kernelSize_List, stride_Integer, paddingSize_Integer ] :=
+BNConvLayer[ outputChannels_Integer, kernelSize_List, stride_Integer, paddingSize_Integer ] :=
    NetChain[{ConvolutionLayer[ outputChannels, kernelSize, "Stride"->stride, "PaddingSize"->paddingSize ],
    BatchNormalizationLayer[]
 }];
 
 
-RetinaNetBranch2[ outputChannels_, stride_ ] := NetChain[ {
-   "a"->{BNConvolutionLayer[ outputChannels/4, {1,1}, stride, 0 ], Ramp},
-   "b"->{BNConvolutionLayer[ outputChannels/4, {3,3}, 1, 1 ], Ramp},
-   "c"->BNConvolutionLayer[ outputChannels, {1,1}, 1, 0 ]
+ResBranch2[ outputChannels_, stride_ ] := NetChain[ {
+   "a"->{BNConvLayer[ outputChannels/4, {1,1}, stride, 0 ], Ramp},
+   "b"->{BNConvLayer[ outputChannels/4, {3,3}, 1, 1 ], Ramp},
+   "c"->BNConvLayer[ outputChannels, {1,1}, 1, 0 ]
 }];
 
 
-RetinaNetBlock[ outputChannels_, stride_, batchNormBranch1_Symbol: False ] := NetGraph[{
-   If [batchNormBranch1, "branch1"->BNConvolutionLayer[ outputChannels, {1,1}, stride, 0 ], Nothing ],
-   "branch2"->RetinaNetBranch2[ outputChannels, stride ],
+ResBlock[ outputChannels_, stride_, batchNormBranch1_Symbol: False ] := NetGraph[{
+   If [batchNormBranch1, "branch1"->BNConvLayer[ outputChannels, {1,1}, stride, 0 ], Nothing ],
+   "branch2"->ResBranch2[ outputChannels, stride ],
    "sum"->TotalLayer[],
    "relu"->Ramp},
    {{If[batchNormBranch1,"branch1",NetPort["Input"]],"branch2"}->"sum"->"relu"}]
 
 
-ResidualNetBlock[ repeats_, channels_, initStride_] := NetChain[Prepend[
-   ToString[0]->RetinaNetBlock[ channels, initStride, True ]][
-   Table[ ToString[k]->RetinaNetBlock[ channels, 1 ], {k, repeats} ]
+ResChain[ repeats_, channels_, initStride_] := NetChain[Prepend[
+   ToString[0]->ResBlock[ channels, initStride, True ]][
+   Table[ ToString[k]->ResBlock[ channels, 1 ], {k, repeats} ]
 ]]
 
 
 ResBackboneNet = NetGraph[{
-   "conv1"->{BNConvolutionLayer[ 64, {7,7}, 2, 3 ], Ramp},
+   "conv1"->{BNConvLayer[ 64, {7,7}, 2, 3 ], Ramp},
    "pool1"->PoolingLayer[ {3,3}, "Stride"->2, "PaddingSize"->1 ],   
-   "res2"->ResidualNetBlock[ 2, 256, 1 ],
-   "res3"->ResidualNetBlock[ 3, 512, 2 ],
-   "res4"->ResidualNetBlock[ 22, 1024, 2 ],
-   "res5"->ResidualNetBlock[ 2, 2048, 2 ]
+   "res2"->ResChain[ 2, 256, 1 ],
+   "res3"->ResChain[ 3, 512, 2 ],
+   "res4"->ResChain[ 22, 1024, 2 ],
+   "res5"->ResChain[ 2, 2048, 2 ]
 },{
    "conv1"->"pool1"->"res2"->"res3"->"res4"->"res5",
    "res3"->NetPort["res3_3_sum"],
@@ -72,23 +72,23 @@ FPNNet = NetGraph[{
 
 MultiBoxDecoderNet = NetGraph[{
    "ClassDecoder"->{
-      "retnet_cls_conv_n0_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
-      "retnet_cls_conv_n1_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
-      "retnet_cls_conv_n2_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
-      "retnet_cls_conv_n3_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
-      "retnet_cls_pred_fpn"->ConvolutionLayer[ 720, {3,3}, "PaddingSize"->1 ],
-      "retnet_cls_pred_prob"->LogisticSigmoid,
+      "conv_n0_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
+      "conv_n1_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
+      "conv_n2_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
+      "conv_n3_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
+      "pred_fpn"->ConvolutionLayer[ 720, {3,3}, "PaddingSize"->1 ],
+      "pred_prob"->LogisticSigmoid,
       "flatten"->{TransposeLayer[{1->2,2->3}],ReshapeLayer[{Inherited,Inherited,9,80}],FlattenLayer[2]}},
    "BoxesDecoder"->{
-      "retnet_bbox_conv_n0_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
-      "retnet_bbox_conv_n1_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
-      "retnet_bbox_conv_n2_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
-      "retnet_bbox_conv_n3_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
-      "retnet_bbox_pred_fpn"->ConvolutionLayer[ 36, {3,3}, "PaddingSize"->1 ],
+      "conv_n0_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
+      "conv_n1_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
+      "conv_n2_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
+      "conv_n3_fpn"->{ConvolutionLayer[ 256, {3,3}, "PaddingSize"->1 ],Ramp},
+      "pred_fpn"->ConvolutionLayer[ 36, {3,3}, "PaddingSize"->1 ],
       "flatten"->{TransposeLayer[{1->2,2->3}],ReshapeLayer[{Inherited,Inherited,9,4}],FlattenLayer[2]}}
       },{
    "ClassDecoder"->NetPort["ClassProb"],
-   "BoxesDecoder"->NetPort["Boxes"]}];
+   "BoxesDecoder"->NetPort["Locs"]}];
 
 
 DecoderNet =
@@ -98,16 +98,16 @@ DecoderNet =
       "multibox5"->MultiBoxDecoderNet,
       "multibox6"->MultiBoxDecoderNet,
       "multibox7"->MultiBoxDecoderNet,
-      "catenate1"->CatenateLayer[],
-      "catenate2"->CatenateLayer[]
+      "ClassProb"->CatenateLayer[],
+      "Locs"->CatenateLayer[]
 },{
       NetPort["multibox3"]->"multibox3",
       NetPort["multibox4"]->"multibox4",
       NetPort["multibox5"]->"multibox5",
       NetPort["multibox6"]->"multibox6",
       NetPort["multibox7"]->"multibox7",
-      {NetPort["multibox3","ClassProb"],NetPort["multibox4","ClassProb"],NetPort["multibox5","ClassProb"],NetPort["multibox6","ClassProb"],NetPort["multibox7","ClassProb"]}->"catenate1"->NetPort["ClassProb"],
-   {NetPort["multibox3","Boxes"],NetPort["multibox4","Boxes"],NetPort["multibox5","Boxes"],NetPort["multibox6","Boxes"],NetPort["multibox7","Boxes"]}->"catenate2"->NetPort["Locs"]
+      {NetPort["multibox3","ClassProb"],NetPort["multibox4","ClassProb"],NetPort["multibox5","ClassProb"],NetPort["multibox6","ClassProb"],NetPort["multibox7","ClassProb"]}->"ClassProb"->NetPort["ClassProb"],
+   {NetPort["multibox3","Locs"],NetPort["multibox4","Locs"],NetPort["multibox5","Locs"],NetPort["multibox6","Locs"],NetPort["multibox7","Locs"]}->"Locs"->NetPort["Locs"]
 }];
 
 
@@ -120,26 +120,26 @@ BoxTransformationNet = NetGraph[ { (*input is in format {Y*X*A}*4*)
    "miny"->ThreadingLayer[896+1-(#1+#2/2)&],
    "maxx"->ThreadingLayer[#1+#2/2&],
    "maxy"->ThreadingLayer[896+1-(#1-#2/2)&],
-   "cat"->CatenateLayer[],"reshape"->ReshapeLayer[ {4, 193347} ], "transpose"->TransposeLayer[], "reshapePoint"->ReshapeLayer[ {193347, 2, 2 } ] }, {
+   "Boxes"->CatenateLayer[],"reshape"->ReshapeLayer[ {4, 193347} ], "transpose"->TransposeLayer[], "reshapePoint"->ReshapeLayer[ {193347, 2, 2 } ] }, {
    {"cx","width"}->"minx",{"cx","width"}->"maxx",{"cy","height"}->"miny",{"cy","height"}->"maxy",
-   {"minx","miny","maxx","maxy"}->"cat"->"reshape"->"transpose"->"reshapePoint"->NetPort["Boxes"]}];
+   {"minx","miny","maxx","maxy"}->"Boxes"->"reshape"->"transpose"->"reshapePoint"->NetPort["Boxes"]}];
 
 
 RetinaNet = NetGraph[ {
-   "ResBackboneNet"->ResBackboneNet,
-   "FPNNet"->FPNNet,
-   "DecoderNet"->DecoderNet,
+   "ResBackbone"->ResBackboneNet,
+   "FPN"->FPNNet,
+   "Decoder"->DecoderNet,
    "BoxTransformation"->BoxTransformationNet},
 {
-   NetPort["ResBackboneNet","res3_3_sum"]->NetPort["FPNNet","res3_3_sum"],
-   NetPort["ResBackboneNet","res4_22_sum"]->NetPort["FPNNet","res4_22_sum"],
-   NetPort["ResBackboneNet","res5_2_sum"]->NetPort["FPNNet","res5_2_sum"],
-   NetPort["FPNNet","multibox3"]->NetPort["DecoderNet","multibox3"],
-   NetPort["FPNNet","multibox4"]->NetPort["DecoderNet","multibox4"],
-   NetPort["FPNNet","multibox5"]->NetPort["DecoderNet","multibox5"],
-   NetPort["FPNNet","multibox6"]->NetPort["DecoderNet","multibox6"],
-   NetPort["FPNNet","multibox7"]->NetPort["DecoderNet","multibox7"],
-   NetPort["DecoderNet","Locs"]->"BoxTransformation"
+   NetPort["ResBackbone","res3_3_sum"]->NetPort["FPN","res3_3_sum"],
+   NetPort["ResBackbone","res4_22_sum"]->NetPort["FPN","res4_22_sum"],
+   NetPort["ResBackbone","res5_2_sum"]->NetPort["FPN","res5_2_sum"],
+   NetPort["FPN","multibox3"]->NetPort["Decoder","multibox3"],
+   NetPort["FPN","multibox4"]->NetPort["Decoder","multibox4"],
+   NetPort["FPN","multibox5"]->NetPort["Decoder","multibox5"],
+   NetPort["FPN","multibox6"]->NetPort["Decoder","multibox6"],
+   NetPort["FPN","multibox7"]->NetPort["Decoder","multibox7"],
+   NetPort["Decoder","Locs"]->"BoxTransformation"
 
    },
  "Input"->NetEncoder[{"Image",{1152,896},"ColorSpace"->"RGB","MeanImage"->Reverse@{102.9801, 115.9465, 122.7717}/256.}]];
--- ModelConverters/RetinaNetInitialization.m
+++ ModelConverters/RetinaNetInitialization.m
@@ -6,88 +6,85 @@ hdfFile = "~/Google Drive/RetinaNetNew1.hdf5";
 imp[ hdfName_String ] := Import[ hdfFile, {"Datasets", hdfName} ];
 
 
-ConvolutionLayerRules[ rootName_, outputChannels_, biases_Symbol: False ] := {
+ConvLayerRules[ rootName_, outputChannels_, biases_Symbol: False ] := {
    {"Weights"}->imp[rootName<>"_w"], {"Biases"}->If[ biases, imp[rootName<>"_b"], ConstantArray[0,Length@imp[rootName<>"_w"]]]};
 
 
-BNConvolutionLayerRules[ rootName_, outputChannels_ ] := {
+BNConvLayerRules[ rootName_, outputChannels_ ] := {
    {1,"Weights"}->imp[rootName<>"_w"], {1,"Biases"}->ConstantArray[0,Length@imp[rootName<>"_w"]],
    {2,"Epsilon"}->.001, {2,"MovingMean"}->ConstantArray[0, outputChannels ], {2,"MovingVariance"}->ConstantArray[ 1.-10^-3, outputChannels ], {2,"Scaling"}->imp[ rootName<>"_bn_s" ], {2,"Biases"}->imp[ rootName<>"_bn_b" ]
 };
 
 
-InsertLevel[ rules_, level_ ] := Map[ Prepend[level][#[[1]]]->#[[2]]&, rules ];
-
-
 InsertLevels[ rules_, levels_ ] := Map[ Join[levels,#[[1]]]->#[[2]]&, rules ];
 
 
-RetinaNetBranch2Rules[ rootName_, outputChannels_ ] := Join[
-   InsertLevels[ BNConvolutionLayerRules[ rootName<>"a", outputChannels/4 ], { "a", 1 } ],
-   InsertLevels[ BNConvolutionLayerRules[ rootName<>"b", outputChannels/4 ], { "b", 1 } ],
-   InsertLevel[ BNConvolutionLayerRules[ rootName<>"c", outputChannels ], "c" ]
+ResidualBranch2Rules[ rootName_, outputChannels_ ] := Join[
+   InsertLevels[ BNConvLayerRules[ rootName<>"a", outputChannels/4 ], { "a", 1 } ],
+   InsertLevels[ BNConvLayerRules[ rootName<>"b", outputChannels/4 ], { "b", 1 } ],
+   InsertLevels[ BNConvLayerRules[ rootName<>"c", outputChannels ], { "c" } ]
 ];
 
 
-RetinaNetBlockRules[ rootName_, outputChannels_, batchNormBranch1_Symbol: False  ] := Join[
-   InsertLevel[ RetinaNetBranch2Rules[ rootName<>"_branch2", outputChannels ], "branch2" ],
-   If[ batchNormBranch1, InsertLevel[ BNConvolutionLayerRules[ rootName<>"_branch1", outputChannels ], "branch1" ], {} ]
+ResidualBlockRules[ rootName_, outputChannels_, batchNormBranch1_Symbol: False  ] := Join[
+   InsertLevels[ ResidualBranch2Rules[ rootName<>"_branch2", outputChannels ], { "branch2" } ],
+   If[ batchNormBranch1, InsertLevels[ BNConvLayerRules[ rootName<>"_branch1", outputChannels ], { "branch1" } ], {} ]
 ];
 
 
 ResidualNetBlockRules[ rootName_, repeats_, outputChannels_ ] := Flatten[Prepend[
-   InsertLevel[ RetinaNetBlockRules[ rootName<>"_0", outputChannels, True ], "0" ]][
-   Table[ InsertLevel[ RetinaNetBlockRules[ rootName<>"_"<>ToString[k], outputChannels ], ToString[k] ], {k, repeats} ]],1];
+   InsertLevels[ ResidualBlockRules[ rootName<>"_0", outputChannels, True ], { "0" } ]][
+   Table[ InsertLevels[ ResidualBlockRules[ rootName<>"_"<>ToString[k], outputChannels ], { ToString[k] } ], {k, repeats} ]],1];
 
 
 ResBackboneNetRules = Join[
-   InsertLevels[ BNConvolutionLayerRules[ "conv1", 64 ], {"conv1",1} ],
-   InsertLevel[ ResidualNetBlockRules[ "res2", 2, 256 ], "res2" ],
-   InsertLevel[ ResidualNetBlockRules[ "res3", 3, 512 ], "res3" ],
-   InsertLevel[ ResidualNetBlockRules[ "res4", 22, 1024 ], "res4" ],
-   InsertLevel[ ResidualNetBlockRules[ "res5", 2, 2048 ], "res5" ]   
+   InsertLevels[ BNConvLayerRules[ "conv1", 64 ], { "conv1", 1} ],
+   InsertLevels[ ResidualNetBlockRules[ "res2", 2, 256 ], { "res2" } ],
+   InsertLevels[ ResidualNetBlockRules[ "res3", 3, 512 ], { "res3" } ],
+   InsertLevels[ ResidualNetBlockRules[ "res4", 22, 1024 ], { "res4" } ],
+   InsertLevels[ ResidualNetBlockRules[ "res5", 2, 2048 ], { "res5" } ]   
 ];
 
 
 FPNNetRules = Join[
-   InsertLevel[ ConvolutionLayerRules[ "fpn_inner_res3_3_sum_lateral", 256, True ], "inner_res3_3_sum_lateral" ],
-   InsertLevel[ ConvolutionLayerRules[ "fpn_inner_res4_22_sum_lateral", 256, True ], "inner_res4_22_sum_lateral" ],
-   InsertLevel[ ConvolutionLayerRules[ "fpn_inner_res5_2_sum", 256, True ], "inner_res5_2_sum" ],
-   InsertLevel[ ConvolutionLayerRules[ "fpn_res3_3_sum", 256, True ], "res3_3_sum" ],
-   InsertLevel[ ConvolutionLayerRules[ "fpn_res4_22_sum", 256, True ], "res4_22_sum" ],
-   InsertLevel[ ConvolutionLayerRules[ "fpn_res5_2_sum", 256, True ], "res5_2_sum" ],
-   InsertLevel[ ConvolutionLayerRules[ "fpn_6", 256, True ], "6" ],
-   InsertLevels[ ConvolutionLayerRules[ "fpn_7", 256, True ], {"7", 2} ]
+   InsertLevels[ ConvLayerRules[ "fpn_inner_res3_3_sum_lateral", 256, True ], { "inner_res3_3_sum_lateral" } ],
+   InsertLevels[ ConvLayerRules[ "fpn_inner_res4_22_sum_lateral", 256, True ], { "inner_res4_22_sum_lateral" } ],
+   InsertLevels[ ConvLayerRules[ "fpn_inner_res5_2_sum", 256, True ], { "inner_res5_2_sum" } ],
+   InsertLevels[ ConvLayerRules[ "fpn_res3_3_sum", 256, True ], { "res3_3_sum" } ],
+   InsertLevels[ ConvLayerRules[ "fpn_res4_22_sum", 256, True ], { "res4_22_sum" } ],
+   InsertLevels[ ConvLayerRules[ "fpn_res5_2_sum", 256, True ], { "res5_2_sum" } ],
+   InsertLevels[ ConvLayerRules[ "fpn_6", 256, True ], { "6" } ],
+   InsertLevels[ ConvLayerRules[ "fpn_7", 256, True ], {"7", 2} ]
 ];
 
 
 MultiBoxDecoderNetRules = Join[
-   InsertLevels[ ConvolutionLayerRules[ "retnet_cls_conv_n0_fpn3", 256, True ], {"ClassDecoder", "retnet_cls_conv_n0_fpn",1} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_cls_conv_n1_fpn3", 256, True ], {"ClassDecoder", "retnet_cls_conv_n1_fpn",1} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_cls_conv_n2_fpn3", 256, True ], {"ClassDecoder", "retnet_cls_conv_n2_fpn",1} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_cls_conv_n3_fpn3", 256, True ], {"ClassDecoder", "retnet_cls_conv_n3_fpn",1} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_cls_pred_fpn3", 256, True ], {"ClassDecoder", "retnet_cls_pred_fpn"} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_bbox_conv_n0_fpn3", 256, True ], {"BoxesDecoder", "retnet_bbox_conv_n0_fpn",1} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_bbox_conv_n1_fpn3", 256, True ], {"BoxesDecoder", "retnet_bbox_conv_n1_fpn",1} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_bbox_conv_n2_fpn3", 256, True ], {"BoxesDecoder", "retnet_bbox_conv_n2_fpn",1} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_bbox_conv_n3_fpn3", 256, True ], {"BoxesDecoder", "retnet_bbox_conv_n3_fpn",1} ],
-   InsertLevels[ ConvolutionLayerRules[ "retnet_bbox_pred_fpn3", 256, True ], {"BoxesDecoder", "retnet_bbox_pred_fpn"} ]
+   InsertLevels[ ConvLayerRules[ "retnet_cls_conv_n0_fpn3", 256, True ], {"ClassDecoder", "conv_n0_fpn",1} ],
+   InsertLevels[ ConvLayerRules[ "retnet_cls_conv_n1_fpn3", 256, True ], {"ClassDecoder", "conv_n1_fpn",1} ],
+   InsertLevels[ ConvLayerRules[ "retnet_cls_conv_n2_fpn3", 256, True ], {"ClassDecoder", "conv_n2_fpn",1} ],
+   InsertLevels[ ConvLayerRules[ "retnet_cls_conv_n3_fpn3", 256, True ], {"ClassDecoder", "conv_n3_fpn",1} ],
+   InsertLevels[ ConvLayerRules[ "retnet_cls_pred_fpn3", 256, True ], { "ClassDecoder", "pred_fpn"} ],
+   InsertLevels[ ConvLayerRules[ "retnet_bbox_conv_n0_fpn3", 256, True ], {"BoxesDecoder", "conv_n0_fpn",1} ],
+   InsertLevels[ ConvLayerRules[ "retnet_bbox_conv_n1_fpn3", 256, True ], {"BoxesDecoder", "conv_n1_fpn",1} ],
+   InsertLevels[ ConvLayerRules[ "retnet_bbox_conv_n2_fpn3", 256, True ], {"BoxesDecoder", "conv_n2_fpn",1} ],
+   InsertLevels[ ConvLayerRules[ "retnet_bbox_conv_n3_fpn3", 256, True ], {"BoxesDecoder", "conv_n3_fpn",1} ],
+   InsertLevels[ ConvLayerRules[ "retnet_bbox_pred_fpn3", 256, True ], {"BoxesDecoder", "pred_fpn"} ]
 ];
 
 
 DecoderNetRules = Join[
-   InsertLevel[ MultiBoxDecoderNetRules, "multibox3" ],
-   InsertLevel[ MultiBoxDecoderNetRules, "multibox4" ],
-   InsertLevel[ MultiBoxDecoderNetRules, "multibox5" ],
-   InsertLevel[ MultiBoxDecoderNetRules, "multibox6" ],
-   InsertLevel[ MultiBoxDecoderNetRules, "multibox7" ]
+   InsertLevels[ MultiBoxDecoderNetRules, { "multibox3" } ],
+   InsertLevels[ MultiBoxDecoderNetRules, { "multibox4" } ],
+   InsertLevels[ MultiBoxDecoderNetRules, { "multibox5" } ],
+   InsertLevels[ MultiBoxDecoderNetRules, { "multibox6" } ],
+   InsertLevels[ MultiBoxDecoderNetRules, { "multibox7" } ]
 ];
 
 
 anch = Import[ hdfFile, {"Datasets", "anchors"} ];
 
 
-levels={{112,144},{56,72},{28,36},{14,18},{7,9}};
+levels = {{112,144},{56,72},{28,36},{14,18},{7,9}};
 strides = {8,16,32,64,128};
 biasesx = Flatten[Table[(x-1)*strides[[l]]+Mean[anch[[l,a,{1,3}]]],{l,1,5},{y,1,levels[[l,1]]},{x,1,levels[[l,2]]},{a,1,9}]];
 biasesy = Flatten[Table[(y-1)*strides[[l]]+Mean[anch[[l,a,{2,4}]]],{l,1,5},{y,1,levels[[l,1]]},{x,1,levels[[l,2]]},{a,1,9}]];
@@ -106,10 +103,10 @@ BoxTransformationNetRules = {
 
 
 RetinaNetRules = Join[
-   InsertLevel[ ResBackboneNetRules, "ResBackboneNet" ],
-   InsertLevel[ FPNNetRules, "FPNNet" ],
-   InsertLevel[ DecoderNetRules, "DecoderNet" ],
-   InsertLevel[ BoxTransformationNetRules, "BoxTransformation" ]
+   InsertLevels[ ResBackboneNetRules, { "ResBackbone" } ],
+   InsertLevels[ FPNNetRules, { "FPN" } ],
+   InsertLevels[ DecoderNetRules, { "Decoder" } ],
+   InsertLevels[ BoxTransformationNetRules, { "BoxTransformation" } ]
 ];
 
 
