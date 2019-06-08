503b41a9f0fabedbfc30c039fa2ff513e2649927
Julian Francis <julian.w.francis@gmail.com>
2019-06-08 09:24:22 +0100

    Behaviour change to extracting number plate: use MaxOverlapFraction->0.0. We don't want eg small letters from number plate to be extracted as then they won't be blurred enough and that non blurred fragment could be superimposed back on the image undoing the blur

--- CZHighlightPreservePrivacy.m
+++ CZHighlightPreservePrivacy.m
@@ -7,11 +7,11 @@
 
 
 CZPrivacyRects[ image_ ]:= Union[
-   CZDetectYoloOpenImages[image,DetectionClasses->{"Vehicle registration plate"},AcceptanceThreshold->.005][[All,1]],
+   CZDetectYoloOpenImages[image,DetectionClasses->{"Vehicle registration plate"},MaxOverlapFraction->0,AcceptanceThreshold->.005][[All,1]],
    FindFaces[image]]
 
 
-replace[image_,rect_] := Raster[ImageData[GaussianFilter[ImageTrim[image,rect],Min[rect[[2,1]]-rect[[1,1]],rect[[2,2]]-rect[[1,2]]]/5],DataReversed->True],List@@rect]
+replace[image_,rect_] := Raster[ImageData[GaussianFilter[ImageTrim[image,rect],Min[rect[[2,1]]-rect[[1,1]],rect[[2,2]]-rect[[1,2]]]/4],DataReversed->True],List@@rect]
 
 
 CZHighlightPreservePrivacy[ image_Image ] := HighlightImage[ image, Graphics@{replace[image,#]&/@CZPrivacyRects[ image ], CZDisplayObject/@CZDetectObjects[ image, Method->"RetinaNet" ] } ]
