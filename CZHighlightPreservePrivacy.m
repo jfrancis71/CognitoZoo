650302cfb9fb0fe961079ff621497c588f8a1a03
Julian Francis <julian.w.francis@gmail.com>
2019-07-04 09:10:29 +0100

    Adding colourisation support for displaying the bounding boxes

--- CZHighlightPreservePrivacy.m
+++ CZHighlightPreservePrivacy.m
@@ -14,4 +14,4 @@ CZPrivacyRects[ image_ ]:= Union[
 replace[image_,rect_] := Raster[ImageData[GaussianFilter[ImageTrim[image,rect],Min[rect[[2,1]]-rect[[1,1]],rect[[2,2]]-rect[[1,2]]]/4],DataReversed->True],List@@rect]
 
 
-CZHighlightPreservePrivacy[ image_Image ] := HighlightImage[ image, Graphics@{replace[image,#]&/@CZPrivacyRects[ image ], CZDisplayObjects@CZDetectObjects[ image, Method->"RetinaNet" ] } ]
+CZHighlightPreservePrivacy[ image_Image ] := HighlightImage[ image, {replace[image,#]&/@CZPrivacyRects[ image ], CZDisplayObjects[ CZDetectObjects[ image, Method->"RetinaNet" ], CZCmap ] } ]
