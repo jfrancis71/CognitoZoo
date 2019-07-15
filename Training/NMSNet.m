d8bfc121a72244053badb650a50a09f6f723580d
Julian <julian.w.francis@gmail.com>
2019-07-15 16:39:33 +0100

    Moving to VGA, ie 640x480

--- Training/NMSNet.m
+++ Training/NMSNet.m
@@ -23,7 +23,7 @@ decode1 = NetGraph[{
 },{
    "j"->"c1"->"r1"->"c2"->"f"->"o",
    {NetPort["internal"],NetPort["conditional"]}->"j"
-},"internal"->{256,16,16},"conditional"->{9,16,16}];
+},"internal"->{256,15,20},"conditional"->{9,15,20}];
 
 
 decode2 = NetGraph[{
@@ -33,7 +33,7 @@ decode2 = NetGraph[{
 },{
    "j"->"c1"->"r1"->"c2"->"f"->"o",
    {NetPort["internal"],NetPort["conditional"]}->"j"
-},"internal"->{256,8,8},"conditional"->{13,8,8}];
+},"internal"->{256,8,10},"conditional"->{13,8,10}];
 
 
 decode3 = NetGraph[{
@@ -43,12 +43,12 @@ decode3 = NetGraph[{
 },{
    "j"->"c1"->"r1"->"c2"->"f"->"o",
    {NetPort["internal"],NetPort["conditional"]}->"j"
-},"internal"->{256,8,8},"conditional"->{9,8,8}];
+},"internal"->{256,8,10},"conditional"->{9,8,10}];
 
 
 n1 = NetGraph[{
    "l1"->trunk,
-   "l2"->{ConvolutionLayer[256,{3,3},"PaddingSize"->1],Ramp,PoolingLayer[{2,2},"Stride"->2]},
+   "l2"->{PaddingLayer[{{0,0},{0,1},{0,1}}], ConvolutionLayer[256,{3,3},"PaddingSize"->1],Ramp,PoolingLayer[{2,2},"Stride"->2]},
    "l3"->{ConvolutionLayer[256,{3,3},"PaddingSize"->1],Ramp},
    "decode1"->decode1,
    "decode2"->decode2,
@@ -66,7 +66,7 @@ n1 = NetGraph[{
    NetPort[{"decode2","Output"}]->NetPort["Medium"],
    NetPort[{"decode3","Output"}]->NetPort["Large"]
 },
-   "Input"->NetEncoder[{"Image",{512,512},"ColorSpace"->"RGB"}]];
+   "Input"->NetEncoder[{"Image",{640,480},"ColorSpace"->"RGB"}]];
 
 
 (* Works well not quite right train/valid split but in practice works nicely with validation error .001 *)
@@ -84,13 +84,13 @@ CZCentroidsToArray[ centroids_, inputDims_, arrayDims_, stride_, offset_ ] :=
 
 encode[rects_]:=(
 
-   s1 = CZCentroidsToArray[ RegionCentroid/@Select[rects,size[#]<108&], { 512, 512 }, { 16, 16 }, 32, 0 ];
-   m1 = CZCentroidsToArray[ RegionCentroid/@Select[rects,size[#]>=108&&size[#]<=155&], { 512, 512 }, { 8, 8 }, 64, 0 ];
-   l1 = CZCentroidsToArray[ RegionCentroid/@Select[rects,size[#]>155&], { 512, 512 }, { 8, 8 }, 64, 0 ];
+   s1 = CZCentroidsToArray[ RegionCentroid/@Select[rects,size[#]<108&], { 640, 480 }, { 15, 20 }, 32, 0 ];
+   m1 = CZCentroidsToArray[ RegionCentroid/@Select[rects,size[#]>=108&&size[#]<=155&], { 640, 480 }, { 8, 10 }, 64, 0 ];
+   l1 = CZCentroidsToArray[ RegionCentroid/@Select[rects,size[#]>155&], { 640, 480 }, { 8, 10 }, 64, 0 ];
    cond1s = Transpose[ImageData[ImageFilter[Delete[Flatten[#],5]&,Image[s1],1]],{2,3,1}];
-   cond1 = Append[cond1s,First@ResizeLayer[{16,16}][{m1}]];
+   cond1 = Append[cond1s,(First@ResizeLayer[{Scaled[2],Scaled[2]},"Resampling"->"Nearest"][{m1}])[[;;15]]];
    cond2s = Transpose[ImageData[ImageFilter[Delete[Flatten[#],5]&,Image[m1],1]],{2,3,1}];
-   cond2 = Append[Join[cond2s, Transpose[Map[Flatten,Partition[s1,{2,2}],{2}], {2,3,1} ] ], l1];
+   cond2 = Append[Join[cond2s, Transpose[Map[Flatten,Partition[s1,{2,2},2,1,0],{2}], {2,3,1} ] ], l1];
    cond3s = Transpose[ImageData[ImageFilter[Delete[Flatten[#],5]&,Image[l1],1]],{2,3,1}];
    cond3 = Append[cond3s,m1];
    Association[
@@ -98,38 +98,34 @@ encode[rects_]:=(
 ]);
 
 
-files1=FileNames["~/ImageDataSets/FaceScrub/ActorImages/512/ActorImages1/*.jpg"];
-files2=FileNames["~/ImageDataSets/FaceScrub/ActorImages/512/ActorImages2/*.jpg"];
-files3=FileNames["~/ImageDataSets/FaceScrub/ActorImages/512/ActorImages3/*.jpg"];
-files4=FileNames["~/ImageDataSets/FaceScrub/ActorImages/512/ActorImages4/*.jpg"];
-files5=FileNames["~/ImageDataSets/FaceScrub/ActorImages/512/ActorImages5/*.jpg"];
-files6=FileNames["~/ImageDataSets/FaceScrub/ActorImages/512/ActorImages6/*.jpg"];
-files7=FileNames["~/ImageDataSets/FaceScrub/ActressImages/512/ActressImages1/*.jpg"];
-files8=FileNames["~/ImageDataSets/FaceScrub/ActressImages/512/ActressImages2/*.jpg"];
-files9=FileNames["~/ImageDataSets/FaceScrub/ActressImages/512/ActressImages3/*.jpg"];
-files10=FileNames["~/ImageDataSets/FaceScrub/ActressImages/512/ActressImages4/*.jpg"];
-files11=FileNames["~/ImageDataSets/FaceScrub/ActressImages/512/ActressImages5/*.jpg"];
-files12=FileNames["~/ImageDataSets/FaceScrub/ActressImages/512/ActressImages6/*.jpg"];
-files13=FileNames["~/ImageDataSets/PascalVOC/VOC2007/512/*.jpg"];
-
-files=Join[files1,files2,files3,files4,files5,files6,files7,files8,files9,files10,files11,files12,files13];
+files1=FileNames["~/ImageDataSets/FaceScrub/ActorImages/VGA/ActorImages1/*.jpg"];
+files2=FileNames["~/ImageDataSets/FaceScrub/ActorImages/VGA/ActorImages2/*.jpg"];
+files3=FileNames["~/ImageDataSets/FaceScrub/ActorImages/VGA/ActorImages3/*.jpg"];
+files4=FileNames["~/ImageDataSets/FaceScrub/ActorImages/VGA/ActorImages4/*.jpg"];
+files5=FileNames["~/ImageDataSets/FaceScrub/ActorImages/VGA/ActorImages5/*.jpg"];
+files6=FileNames["~/ImageDataSets/FaceScrub/ActorImages/VGA/ActorImages6/*.jpg"];
+files7=FileNames["~/ImageDataSets/FaceScrub/ActressImages/VGA/ActressImages1/*.jpg"];
+files8=FileNames["~/ImageDataSets/FaceScrub/ActressImages/VGA/ActressImages2/*.jpg"];
+files9=FileNames["~/ImageDataSets/FaceScrub/ActressImages/VGA/ActressImages3/*.jpg"];
+files10=FileNames["~/ImageDataSets/FaceScrub/ActressImages/VGA/ActressImages4/*.jpg"];
+files11=FileNames["~/ImageDataSets/FaceScrub/ActressImages/VGA/ActressImages5/*.jpg"];
+files12=FileNames["~/ImageDataSets/FaceScrub/ActressImages/VGA/ActressImages6/*.jpg"];
+files=Join[files1,files2,files3,files4,files5,files6,files7,files8,files9,files10,files11,files12];
 
 
-faces1=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibFaceDetectActor1.mx"];
-faces2=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibFaceDetectActor2.mx"];
-faces3=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibFaceDetectActor3.mx"];
-faces4=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibFaceDetectActor4.mx"];
-faces5=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibFaceDetectActor5.mx"];
-faces6=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibFaceDetectActor6.mx"];
-faces7=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibFaceDetectActress1.mx"];
-faces8=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibFaceDetectActress2.mx"];
-faces9=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibFaceDetectActress3.mx"];
-faces10=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibFaceDetectActress4.mx"];
-faces11=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibFaceDetectActress5.mx"];
-faces12=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibFaceDetectActress6.mx"];
-faces13=Import["~/ImageDataSets/PascalVOC/VOC2007/DLibFaces.mx"];
-
-faces=Join[faces1,faces2,faces3,faces4,faces5,faces6,faces7,faces8,faces9,faces10,faces11,faces12,faces13];
+faces1=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibVGAActor1.mx"];
+faces2=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibVGAActor2.mx"];
+faces3=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibVGAActor3.mx"];
+faces4=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibVGAActor4.mx"];
+faces5=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibVGAActor5.mx"];
+faces6=Import["~/ImageDataSets/FaceScrub/ActorImages/DLibVGAActor6.mx"];
+faces7=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibVGAActress1.mx"];
+faces8=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibVGAActress2.mx"];
+faces9=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibVGAActress3.mx"];
+faces10=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibVGAActress4.mx"];
+faces11=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibVGAActress5.mx"];
+faces12=Import["~/ImageDataSets/FaceScrub/ActressImages/DLibVGAActress6.mx"];
+faces=Join[faces1,faces2,faces3,faces4,faces5,faces6,faces7,faces8,faces9,faces10,faces11,faces12];
 
 
 dataset=Table[Append[encode[faces1[[k]]],"Input"->File[files1[[k]]]],{k,1,Length[files1]}];
@@ -138,16 +134,7 @@ dataset=Table[Append[encode[faces1[[k]]],"Input"->File[files1[[k]]]],{k,1,Length
 SeedRandom[1234];rnds=RandomSample[dataset];
 
 
-{trainingSet,validationSet}={rnds[[;;90000]],rnds[[90001;;]]};
-
-
 {trainingSet,validationSet}={rnds[[;;7000]],rnds[[7001;;]]};
-
-
-{trainingSet,validationSet}={rnds[[;;400]],rnds[[401;;]]};
-
-
-Length[dataset]
 
 
 (*
@@ -216,11 +203,12 @@ totdec[assoc_]:=render[ decoder[ assoc ] ]
 (*vis[img_,sofar_]:=( *)
 (*s1=sofar[[1]];m1=sofar[[2]];l1=sofar[[3]];*)
 (*   cond1s = Transpose[ImageData[ImageFilter[Delete[Flatten[#],5]&,Image[s1],1]],{2,3,1}];*)
-(*   cond1 = Append[cond1s,First@ResizeLayer[{16,16}][{m1}]];*)
+(*   cond1 = Append[cond1s,(First@ResizeLayer[{Scaled[2],Scaled[2]},"Resampling"->"Nearest"][{m1}])[[;;15]]];*)
 (*   cond2s = Transpose[ImageData[ImageFilter[Delete[Flatten[#],5]&,Image[m1],1]],{2,3,1}];*)
-(*   cond2 = Append[Join[cond2s, Transpose[Map[Flatten,Partition[s1,{2,2}],{2}], {2,3,1} ] ], l1];*)
+(*   cond2 = Append[Join[cond2s, Transpose[Map[Flatten,Partition[s1,{2,2},2,1,0],{2}], {2,3,1} ] ], l1];*)
 (*   cond3s = Transpose[ImageData[ImageFilter[Delete[Flatten[#],5]&,Image[l1],1]],{2,3,1}];*)
 (*   cond3 = Append[cond3s,m1];*)
+(**)
 (**)
 (*r=net2[Association[*)
 (*"Input"->img,*)
@@ -237,14 +225,14 @@ render[ result_ ] := Join[
    
 
    Map[
-         Rectangle[{32*(#[[2]]-.5),512-32*(#[[1]]-.5)}-{37,37},{32*(#[[2]]-.5),512-32*(#[[1]]-.5)}+{37,37}]&,
+         Rectangle[{32*(#[[2]]-.5),480-32*(#[[1]]-.5)}-{37,37},{32*(#[[2]]-.5),480-32*(#[[1]]-.5)}+{37,37}]&,
       Position[result[[1]],x_/;x>0]],
    Map[   
-      Rectangle[{64*(#[[2]]-.5),512-64*(#[[1]]-.5)}-{65,65}/2,{64*(#[[2]]-.5),512-64*(#[[1]]-.5)}+{65,65}/2]&,
+      Rectangle[{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}-{65,65}/2,{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}+{65,65}/2]&,
       Position[result[[2]],x_/;x>0]],
          
          Map[
-      Rectangle[{64*(#[[2]]-.5),512-64*(#[[1]]-.5)}-{100,100},{64*(#[[2]]-.5),512-64*(#[[1]]-.5)}+{100,100}]&,
+      Rectangle[{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}-{100,100},{64*(#[[2]]-.5),480-64*(#[[1]]-.5)}+{100,100}]&,
       Position[result[[3]],x_/;x>0]]
 
       ];      
