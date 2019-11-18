02a447ce1910c969860fac804535b1019c3ae582
Julian Francis <julian.w.francis@gmail.com>
2019-11-18 12:48:35 +0800

    removing functional style

--- Experimental/GenerativeModels/PixelVaE.m
+++ Experimental/GenerativeModels/PixelVaE.m
@@ -1,5 +1,11 @@
 (* ::Package:: *)
 
+<<Experimental/GenerativeModels/PixelCNN.m
+
+
+<<"Experimental/GenerativeModels/VariationalAutoencoder.m"
+
+
 CZCreatePixelVaEDecoder[ inputUnits_, h1_, h2_ ] := NetGraph[{
       "h1"->{h1,Ramp},
       "h2"->{h2,Ramp},
@@ -12,18 +18,17 @@ CZCreatePixelVaEDecoder[ inputUnits_, h1_, h2_ ] := NetGraph[{
 }]
 
 
-SyntaxInformation[ PixelVaEAutoencoder ]= {"ArgumentsPattern"->{_,_,_}};
+SyntaxInformation[ PixelVaE ]= {"ArgumentsPattern"->{_,_,_}};
 
 
-CZCreatePixelVaE[ inputUnits_, latentUnits_, h1_:500, h2_:500 ] := PixelVaEAutoencoder[
+CZCreatePixelVaE[ inputUnits_, latentUnits_, h1_:500, h2_:500 ] := PixelVaE[
    inputUnits,
    latentUnits,
    NetGraph[{
       "encoder"->CZCreateEncoder[ inputUnits, latentUnits, h1, h2 ],
       "sampler"->CZCreateSampler[],
       "decoder"->CZCreatePixelVaEDecoder[ inputUnits, h1, h2 ],
-      "kl_loss"->CZKLLoss,
-      "decoder"->CZCreatePixelVaEDecoder[ inputUnits, h1, h2 ]
+      "kl_loss"->CZKLLoss
       },{
       NetPort[{"encoder","Mean"}]->{NetPort[{"sampler","Mean"}],NetPort[{"kl_loss","Mean"}],NetPort["Mean"]},
       NetPort[{"encoder","LogVar"}]->{NetPort[{"sampler","LogVar"}],NetPort[{"kl_loss","LogVar"}],NetPort["LogVar"]},
@@ -35,12 +40,12 @@ CZCreatePixelVaE[ inputUnits_, latentUnits_, h1_:500, h2_:500 ] := PixelVaEAutoe
 ];
 
 
-Train[ PixelVaEAutoencoder[ inputUnits_, latentUnits_, net_ ] ][ examples_ ] := (
+Train[ PixelVaE[ inputUnits_, latentUnits_, net_ ], examples_ ] := (
    f[assoc_] := MapThread[
-      Association["Input"->#1,"RandomSample"->#2]&,
+      Association["Input"->Flatten[#1],"RandomSample"->#2]&,
       {RandomSample[examples,assoc["BatchSize"]],Partition[RandomVariate[NormalDistribution[0,1],latentUnits*assoc["BatchSize"]],latentUnits]}];
 
-   PixelVaEAutoencoder[ inputUnits, latentUnits, NetTrain[ net, f,
+   PixelVaE[ inputUnits, latentUnits, NetTrain[ net, f,
       LearningRateMultipliers->
          Flatten[Table[
          {{"decoder","cond","conv"<>ToString[k],"mask"}->0,{"decoder","cond","loss"<>ToString[k],"mask"}->0},{k,1,Length[pixels]}],1]
@@ -49,7 +54,7 @@ Train[ PixelVaEAutoencoder[ inputUnits_, latentUnits_, net_ ] ][ examples_ ] := 
 ] );
 
 
-Sample[ PixelVaEAutoencoder[ _, _, net_ ] ] := Module[{s=ConstantArray[0,{28,28}],decoder=NetExtract[ net, "decoder" ],cond=NetExtract[ net, {"decoder","cond"}]},tmp=decoder;
+Sample[ PixelVaE[ _, _, net_ ] ] := Module[{s=ConstantArray[0,{28,28}],decoder=NetExtract[ net, "decoder" ],cond=NetExtract[ net, {"decoder","cond"}]},tmp=decoder;
    znet = NetTake[ decoder, "reshapecond" ];
    z = znet[ RandomVariate@MultinormalDistribution[ConstantArray[0,{8}],IdentityMatrix[8] ] ];
    For[k=1,k<=Length[pixels],k++,
