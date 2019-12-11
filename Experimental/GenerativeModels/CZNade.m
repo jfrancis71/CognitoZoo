25327602d2b5d3ed14a5c67b6d5cb69b828d6b4c
Julian Francis <julian.w.francis@gmail.com>
2019-12-11 12:56:29 +0000

    Adding Nade for Real numbers, currently assumes gaussian for each output

--- Experimental/GenerativeModels/CZNade.m
+++ Experimental/GenerativeModels/CZNade.m
@@ -3,17 +3,23 @@
 <<"Experimental/GenerativeModels/CZGenerativeUtils.m"
 
 
-condNadeNetNode[k_] := NetGraph[{PartLayer[;;k-1],CatenateLayer[],k,Tanh,k,Tanh,1,LogisticSigmoid},{
+condNadeNetNode[ k_, outputType_ ] := NetGraph[{PartLayer[;;k-1],CatenateLayer[],k,Tanh,k,Tanh,1,outputType},{
    NetPort["Input"]->1->2,
    NetPort["Conditional"]->2,
    2->3->4->5->6->7->8}]
 
 
-createCondNadeNet[ inputUnits_ ] := NetGraph[Flatten[{
-   "proc1"->{ConstantArrayLayer[{1}],LogisticSigmoid},
-   Table["proc"<>ToString[k]->condNadeNetNode[k],{k,2,inputUnits}],
+condNadeNetNodeRealVector[ k_ ] := NetGraph[{PartLayer[;;k-1],CatenateLayer[],k,Tanh,k,Tanh,2},{
+   NetPort["Input"]->1->2,
+   NetPort["Conditional"]->2,
+   2->3->4->5->6->7}]
+
+
+createCondNadeNet[ inputUnits_, outputType_, lossFunction_ ] := NetGraph[Flatten[{
+   "proc1"->{ConstantArrayLayer[{1}],outputType},
+   Table["proc"<>ToString[k]->condNadeNetNode[ k, outputType ],{k,2,inputUnits}],
    "cat"->CatenateLayer[],
-   "loss"->CrossEntropyLossLayer["Binary"]
+   "loss"->lossFunction
 },1],
    {Table["proc"<>ToString[k]->"cat",{k,1,inputUnits}],
    NetPort["Input"]->NetPort[{"loss","Target"}],
@@ -22,9 +28,53 @@ createCondNadeNet[ inputUnits_ ] := NetGraph[Flatten[{
 ];
 
 
-createNadeNet[ inputUnits_ ] := NetGraph[{
+CZGaussianLossLayer = NetGraph[{
+   "mean"->PartLayer[{All,1}],
+   "logdev"->PartLayer[{All,2}],
+   "neg"->ElementwiseLayer[-#&],
+   "diff"->TotalLayer[],
+   "precision"->ElementwiseLayer[1/Exp[#]&],
+   "mult"->ThreadingLayer[Times],
+   "sq"->ElementwiseLayer[#^2&],
+   "expterm"->ElementwiseLayer[-.5*#&],
+   "normterm"->ElementwiseLayer[Log], (*I've ignored root pi term*)
+   "logpdf"->TotalLayer[],
+   "neglogpdf"->ElementwiseLayer[-#&],
+   "loss"->SummationLayer[]},{
+   NetPort["Target"]->"neg",
+   {"mean","neg"}->"diff",
+   "logdev"->"precision",
+   {"diff","precision"}->"mult"->"sq"->"expterm",
+   "precision"->"normterm",
+   {"normterm","expterm"}->"logpdf"->"neglogpdf"->"loss"->NetPort["Loss"]
+}];
+
+
+createCondNadeRealVectorNet[ inputUnits_ ] := NetGraph[Flatten[{
+   "proc1"->{ConstantArrayLayer[{2}]},
+   Table["proc"<>ToString[k]->condNadeNetNodeRealVector[ k ],{k,2,inputUnits}],
+   "cat"->CatenateLayer[],
+   "reshape"->ReshapeLayer[{inputUnits,2}],
+   "loss"->CZGaussianLossLayer
+},1],
+   {Table["proc"<>ToString[k]->"cat",{k,1,inputUnits}],
+   NetPort["Input"]->NetPort[{"loss","Target"}],
+   "cat"->"reshape",
+   NetPort[{"reshape","Output"}]->{NetPort["Output"],NetPort[{"loss","Input"}]}},
+   "Input"->inputUnits
+];
+
+
+createNadeNet[ inputUnits_, outputType_, lossFunction_ ] := NetGraph[{
    "constant"->ConstantArrayLayer[{1}],
-   "condNadeNet"->createCondNadeNet[ inputUnits ]},{
+   "condNadeNet"->createCondNadeNet[ inputUnits, outputType, lossFunction ]},{
+   "constant"->NetPort[{"condNadeNet","Conditional"}]
+   }];
+
+
+createNadeRealVectorNet[ inputUnits_ ] := NetGraph[{
+   "constant"->ConstantArrayLayer[{1}],
+   "condNadeNet"->createCondNadeRealVectorNet[ inputUnits ]},{
    "constant"->NetPort[{"condNadeNet","Conditional"}]
    }];
 
@@ -32,10 +82,13 @@ createNadeNet[ inputUnits_ ] := NetGraph[{
 SyntaxInformation[ CZNade ]= {"ArgumentsPattern"->{}};
 
 
-CZCreateNade[ inputUnits_: 8] := CZGenerativeModel[ CZNade[], CZBinaryVector[ inputUnits ], Identity, createNadeNet[ inputUnits ] ]
+CZCreateNadeBinaryVector[ inputUnits_: 8] := CZGenerativeModel[ CZNade[], CZBinaryVector[ inputUnits ], Identity, createNadeNet[ inputUnits, LogisticSigmoid, CrossEntropyLossLayer[ "Binary" ] ] ]
 
 
-CZSampleCondNade[ net_, cond_, inputUnits_ ] := (
+CZCreateNadeRealVector[ inputUnits_: 8] := CZGenerativeModel[ CZNade[], CZRealVector[ inputUnits ], Identity, createNadeRealVectorNet[ inputUnits ] ]
+
+
+CZSampleCondNadeBinaryVector[ net_, cond_, inputUnits_ ] := (
    inp=ConstantArray[0,{inputUnits}];
    out=net[ Association[ "Input"->inp, "Conditional"->cond ] ];
    For[k=1,k<=inputUnits,k++,
@@ -46,8 +99,24 @@ CZSampleCondNade[ net_, cond_, inputUnits_ ] := (
 )
 
 
+CZSampleCondNadeRealVector[ net_, cond_, inputUnits_ ] := (
+   inp=ConstantArray[0,{inputUnits}];
+   out=net[ Association[ "Input"->inp, "Conditional"->cond ] ];
+   For[k=1,k<=inputUnits,k++,
+   outm=net[Association[ "Input"->inp, "Conditional"->cond ]]["Output"][[k]];
+   s=RandomVariate[NormalDistribution[outm[[1]],Exp@outm[[2]]]];
+   inp[[k]]=s
+   ];
+   inp
+)
+
+
 CZSample[ CZGenerativeModel[ CZNade[], CZBinaryVector[ inputUnits_ ], id_, net_ ] ] :=
-   CZSampleCondNade[ NetExtract[ net, "condNadeNet" ], NetExtract[ net, "constant" ][], inputUnits ]
+   CZSampleCondNadeBinaryVector[ NetExtract[ net, "condNadeNet" ], NetExtract[ net, "constant" ][], inputUnits ]
+
+
+CZSample[ CZGenerativeModel[ CZNade[], CZRealVector[ inputUnits_ ], id_, net_ ] ] :=
+   CZSampleCondNadeRealVector[ NetExtract[ net, "condNadeNet" ], NetExtract[ net, "constant" ][], inputUnits ]
 
 
 <<"Experimental/GenerativeModels/CZVariationalAutoencoders.m"
