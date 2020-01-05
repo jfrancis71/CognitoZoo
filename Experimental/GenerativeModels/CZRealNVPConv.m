39b7f8898884a136b8d2a948319a07be8384f303
Julian Francis <julian.w.francis@gmail.com>
2020-01-05 11:26:20 +0000

    Adding Squeeze layer.

--- Experimental/GenerativeModels/CZRealNVPConv.m
+++ Experimental/GenerativeModels/CZRealNVPConv.m
@@ -11,9 +11,9 @@ MaskLayer[ mask_ ] := NetGraph[{
    {NetPort["Input"],"mask"}->"thread"}]
 
 
-checkerboard = ConstantArray[0,{1,8,8}];
-checkerboard[[1,1;;8;;2,1;;8;;2]] = 1;
-checkerboard[[1,2;;8;;2,2;;8;;2]] = 1;
+checkerboard8x8 = ConstantArray[0,{1,8,8}];
+checkerboard8x8[[1,1;;8;;2,1;;8;;2]] = 1;
+checkerboard8x8[[1,2;;8;;2,2;;8;;2]] = 1;
 
 
 StableScaleNet = NetGraph[{"nonlinearity"->Tanh,"c1"->ConstantArrayLayer[{1}],"c2"->ConstantArrayLayer[{1}],"times"->ThreadingLayer[Times],"plus"->ThreadingLayer[Plus]},{{"nonlinearity","c1"}->"times",{"times","c2"}->"plus"}];
@@ -45,27 +45,59 @@ CouplingLayer[ passThrough_ ] := NetGraph[{
 }]
 
 
-channelmask = ConstantArray[0,{64,1,1}];
-channelmask[[1;;32,1,1]] = 1;
+channelmask2 = ConstantArray[0,{4,4,4}];
+channelmask2[[1;;2,All,All]] = 1;
+
+
+channelmask64 = ConstantArray[0,{64,1,1}];
+channelmask64[[1;;32,1,1]] = 1;
+
+
+mysqueeze[inChannels_]:=NetGraph[{
+   "s1"->ConvolutionLayer[inChannels,{2,2},"Stride"->2,"Biases"->ConstantArray[0,{inChannels}],"Weights"->Table[
+      If[o==i,{{1,0},{0,0}},{{0,0},{0,0}}],{o,inChannels},{i,inChannels}]],
+   "s2"->ConvolutionLayer[inChannels,{2,2},"Stride"->2,"Biases"->ConstantArray[0,{inChannels}],"Weights"->Table[
+      If[o==i,{{0,1},{0,0}},{{0,0},{0,0}}],{o,inChannels},{i,inChannels}]],
+   "s3"->ConvolutionLayer[inChannels,{2,2},"Stride"->2,"Biases"->ConstantArray[0,{inChannels}],"Weights"->Table[
+      If[o==i,{{0,0},{1,0}},{{0,0},{0,0}}],{o,inChannels},{i,inChannels}]],
+   "s4"->ConvolutionLayer[inChannels,{2,2},"Stride"->2,"Biases"->ConstantArray[0,{inChannels}],"Weights"->Table[
+      If[o==i,{{0,0},{0,1}},{{0,0},{0,0}}],{o,inChannels},{i,inChannels}]],
+   "cat"->CatenateLayer[]
+},{
+{"s1","s2","s3","s4"}->"cat"
+}];
+
+
+RealNVPBlock = NetGraph[{
+   "c1"->CouplingLayer[ checkerboard8x8 ],
+   "c2"->CouplingLayer[ 1 - checkerboard8x8 ],
+   "s1"->mysqueeze[ 1 ],
+   "c3"->CouplingLayer[ channelmask2 ],
+   "c4"->CouplingLayer[ 1 - channelmask2 ],
+   "jacobian"->ThreadingLayer[Plus]},{
+   NetPort[{"c1","Transformed"}]->"c2",
+   NetPort[{"c2","Transformed"}]->"s1",
+   "s1"->"c3",
+   NetPort[{"c3","Transformed"}]->"c4",
+   NetPort[{"c4","Transformed"}]->NetPort["Transformed"],
+   {NetPort["c1","Jacobian"],NetPort["c2","Jacobian"],NetPort["c3","Jacobian"],NetPort["c4","Jacobian"]}->
+      "jacobian"->NetPort["Jacobian"]
+   }];
 
 
 RealNVP = NetGraph[{
-   CouplingLayer[ checkerboard ],
-   CouplingLayer[ 1 - checkerboard ],
-   ReshapeLayer[{64,1,1}],
-   CouplingLayer[ channelmask ],
-   CouplingLayer[ 1 - channelmask ],
-   gauss,
-   ThreadingLayer[Plus],
-   ElementwiseLayer[ -#& ]
+   "block"->RealNVPBlock,   
+   "reshape"->ReshapeLayer[{64,1,1}],
+   "c1"->CouplingLayer[ channelmask ],
+   "c2"->CouplingLayer[ 1 - channelmask ],
+   "g"->gauss,
+   "th"->ThreadingLayer[Plus],
+   "loss"->ElementwiseLayer[ -#& ]
 },{
-   NetPort[{1,"Transformed"}]->2,
-   NetPort[{2,"Transformed"}]->3,
-   3->4,
-   NetPort[{4,"Transformed"}]->5,
-   NetPort[{5,"Transformed"}]->6,
-   {NetPort[{1,"Jacobian"}],NetPort[{2,"Jacobian"}],NetPort[{4,"Jacobian"}],NetPort[{5,"Jacobian"}],6}
-      ->7->8->NetPort["Loss"]
+   NetPort[{"block","Transformed"}]->"reshape"->"c1",
+   NetPort[{"c1","Transformed"}]->"c2",
+   NetPort[{"c2","Transformed"}]->"g",
+   {NetPort[{"block","Jacobian"}],"g",NetPort[{"c1","Jacobian"}],NetPort[{"c2","Jacobian"}]}->"th"->"loss"->NetPort["Loss"]
 },"Input"->{1,8,8}];
 
 
@@ -82,26 +114,42 @@ data1 = data+Table[RandomReal[]/10,{6742},{1},{8},{8}];
 
 
 train = NetTrain[ RealNVP, Association["Input"->#]&/@data1,LearningRateMultipliers->{
+   {"block","s1","s1"}->0,
+   {"block","s1","s2"}->0,
+   {"block","s1","s3"}->0,
+   {"block","s1","s4"}->0,
 
-   {1,"passDirect","mask"}->0,
-   {1,"change","mask"}->0,
-   {1,"changed","mask"}->0,
-   {1,"jacobian2","mask"}->0,
+
+   {"block","c1","passDirect","mask"}->0,
+   {"block","c1","change","mask"}->0,
+   {"block","c1","changed","mask"}->0,
+   {"block","c1","jacobian2","mask"}->0,
+
+   {"block","c2","passDirect","mask"}->0,
+   {"block","c2","change","mask"}->0,
+   {"block","c2","changed","mask"}->0,
+   {"block","c2","jacobian2","mask"}->0,
    
-   {2,"passDirect","mask"}->0,
-   {2,"change","mask"}->0,
-   {2,"changed","mask"}->0,
-   {2,"jacobian2","mask"}->0,
+   {"block","c3","passDirect","mask"}->0,
+   {"block","c3","change","mask"}->0,
+   {"block","c3","changed","mask"}->0,
+   {"block","c3","jacobian2","mask"}->0,
    
-   {4,"passDirect","mask"}->0,
-   {4,"change","mask"}->0,
-   {4,"changed","mask"}->0,
-   {4,"jacobian2","mask"}->0,
-   
-   {5,"passDirect","mask"}->0,
-   {5,"change","mask"}->0,
-   {5,"changed","mask"}->0,
-   {5,"jacobian2","mask"}->0
+   {"block","c4","passDirect","mask"}->0,
+   {"block","c4","change","mask"}->0,
+   {"block","c4","changed","mask"}->0,
+   {"block","c4","jacobian2","mask"}->0,
+
+   {"c1","passDirect","mask"}->0,
+   {"c1","change","mask"}->0,
+   {"c1","changed","mask"}->0,
+   {"c1","jacobian2","mask"}->0,
+
+   {"c2","passDirect","mask"}->0,
+   {"c2","change","mask"}->0,
+   {"c2","changed","mask"}->0,
+   {"c2","jacobian2","mask"}->0
+
 
 
 },LossFunction->"Loss"
@@ -116,12 +164,42 @@ reverse[net_,z_,mask_]:=(
 
 
 sampleFromGaussian[ z_ ] := (
-   z3 = reverse[ NetExtract[ train, {5} ], z, 1-channelmask ];
-   z2 = reverse[ NetExtract[ train, {4} ], z3, channelmask ];
+   oc2i = reverse[ NetExtract[ train, {"c2"} ], z, 1-channelmask ];
+   oc1i = reverse[ NetExtract[ train, {"c1"} ], oc2i, channelmask ];
+   reshape = ReshapeLayer[{4,4,4}][oc1i];
+   (*
    rz2 = ReshapeLayer[{1,8,8}][z2];
    z1 = reverse[ NetExtract[ train, {2} ], rz2, 1-checkerboard ];
-   z0 = reverse[ NetExtract[ train, {1} ], z1, checkerboard ]
+   z0 = reverse[ NetExtract[ train, {1} ], z1, checkerboard ]*)
+)
+
+
+desqueeze[ inp_ ] := (
+   tmp = ConstantArray[0,{1,8,8}];
+   tmp[[1,1;;8;;2,1;;8;;2]] = inp[[1]];
+   tmp[[1,1;;8;;2,2;;8;;2]] = inp[[2]];
+   tmp[[1,2;;8;;2,1;;8;;2]] = inp[[3]];
+   tmp[[1,2;;8;;2,2;;8;;2]] = inp[[4]];
+   tmp
+);
+
+
+sampleBlock[ z_ ] := (
+   c4i = reverse[ NetExtract[ train, {"block","c4"} ], z, 1 - channelmask2 ];
+   c3i = reverse[ NetExtract[ train, {"block","c3"} ], c4i, channelmask2 ];
+   desq = desqueeze[ c3i ];
+   c2i = reverse[ NetExtract[ train, {"block","c2"} ], desq, 1 - checkerboard8x8 ];
+   c1i = reverse[ NetExtract[ train, {"block","c1"} ], c2i, checkerboard8x8 ];
 )
 
 
 gaussiansample[] := Table[RandomVariate@NormalDistribution[0,1],{64},{1},{1}]
+
+
+z=gaussiansample[];
+
+
+sampleFromGaussian[ z ];
+
+
+sampleBlock[ reshape ];
